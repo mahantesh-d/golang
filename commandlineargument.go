c605deedf57cc271a92196c6616f608244dd2642
d-mahantesh <mahantesh.dhale@unotechsoft.com>
2017-06-17 18:00:13 +0700

    taking command line argument and printing

---
+++ commandlineargument.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main()  {
+	arg1 := os.Args
+	arg2 := os.Args[1:]
+	//arg3 := os.Args[3]
+
+	fmt.Print(arg1, "\n")
+	fmt.Print("Hello ",arg2)
+	//fmt.Print(arg3)
+}
