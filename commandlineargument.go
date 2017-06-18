6e7eadb132c34d2ddaa5cde08b610fed4ab61068
d-mahantesh <mahantesh.dhale@unotechsoft.com>
2017-06-18 12:50:22 +0700

    added one more argument

--- commandlineargument.go
+++ commandlineargument.go
@@ -8,9 +8,9 @@ import (
 func main()  {
 	arg1 := os.Args
 	arg2 := os.Args[1:]
-	//arg3 := os.Args[3]
+	arg3 := os.Args[2:]
 
 	fmt.Print(arg1, "\n")
 	fmt.Print("Hello ",arg2)
-	//fmt.Print(arg3)
+	fmt.Print(arg3)
 }
